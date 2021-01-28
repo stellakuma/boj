fc1bbe4d999e32742cce6e63527276366be85e57
Cyanpen <cyanpen13@gmail.com>
2021-01-28 20:42:30 +0900

    11653 add

---
+++ 11654.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main(){
+
+	std::string a;
+	std::cin >> a;
+
+	std::cout << int(a.at(0)) << std::endl;
+
+}
---
+++ 4673.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+
+	int a = 1;
+	int sn = 0;
+	printf(1);
+
+	for( int i = 1; ; i++ ){
+		
+		
+		
+	}
+
+}
+
+int d( int a ){
+	
+	int r = a;
+	int c = 0;
+	
+	for( int i = a; ; i/10 ){
+		
+		if( i < 1 ) break;
+		c += 1;
+		
+	}
+	
+	for( int i = a; ; i/round(pow(10,c)) ){
+		
+		
+		c -= 1;
+		
+	} 
+		
+	return r;
+	
+}
