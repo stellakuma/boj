cb7a9e6f059a7c14b1d8bb597a0049cf0462bc2a
Cyanpen <cyanpen13@gmail.com>
2021-02-15 23:49:34 +0900

    complete 1157

--- 1157.cpp
+++ 1157.cpp
@@ -14,32 +14,45 @@ int main(){
 	    }
 	
 	int d[123] = {0, };
-	//m0 as char, m1 as size
-	int m[2] = {0, };
+	int m[2] = {0, 0};
 
-	//97~122 a~z
 	for( int i = 0; i < s.length(); i++ ){
 		
-		d[(int)s.at(i)] += 1;
+		int nb = (int)s.at(i);
+		d[nb] += 1;
 		
+		if( d[nb] > m[1] ){
+				
+			m[0] = nb;
+			m[1] = d[nb];
+				
+		}
+	
 	}
 	
-	for( int i = 97; ; i++ ){
+	for( int j = 97; ; j++ ){
 		
-		if( d[i] == 0 ) continue;
-		
-		if( d[i] > m[1] ){
+		if( j == 123 ){
 			
-			m[0] = i;
-		
-		} else if( m[1] == d[i] ){
-			
-			//printf("%d %d\n", m[1], d[i]);
-			cout << "?";
+			cout << (char)toupper((char)m[0]) << endl;
 			break;
 			
 		}
 		
-		if ( i == 122 ) cout << toupper((char)m[0]);
+		if( d[j] != 0 ){
+			
+			if( j != m[0] ){
+			
+				if( d[j] == m[1] ){
+					
+					cout << "?" << endl;
+					break;
+					
+				}
+			
+			}
+			
+		}
+		
 	}
 }
