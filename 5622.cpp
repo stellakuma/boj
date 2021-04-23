d132157488a69d6010f1707af0ed8e3dc9a66deb
Cyanpen <cyanpen13@gmail.com>
2021-04-23 22:45:00 +0900

    add 5622.cpp

--- 5622.cpp
+++ 5622.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
+#include <string>
+#include <regex>
+
+using namespace std;
 
 int main(){
-
-	int arr[21] = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765};
-	int n = 0;
-	std::cin >> n;
-	std::cout << arr[n];
-
+	
+	short time = 0;
+	string cn = "";
+	getline( cin, cn );
+	
+	regex r("[A-C]");
+	cn = regex_replace(cn, r, "2");
+	regex r1("[D-F]");
+	cn = regex_replace(cn, r1, "3");
+	regex r2("[G-I]");
+	cn = regex_replace(cn, r2, "4");
+	regex r3("[J-L]");
+	cn = regex_replace(cn, r3, "5");
+	regex r4("[M-O]");
+	cn = regex_replace(cn, r4, "6");
+	regex r5("[P-S]");
+	cn = regex_replace(cn, r5, "7");
+	regex r6("[T-V]");
+	cn = regex_replace(cn, r6, "8");
+	regex r7("[W-Z]");
+	cn = regex_replace(cn, r7, "9");
+	
+	for( short i = 0; i < cn.length(); i++ ){
+		
+		time += cn[i]-47;
+		
+	}
+	
+	cout << time << endl;
 }
